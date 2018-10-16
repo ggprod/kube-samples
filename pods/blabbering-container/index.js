e9739a50c6328d58c60aceac473b5dd9b23deb8c
Mark Pevec <mark.pevec@gmail.com>
2018-10-16 13:39:12 -0400

    update the blabbering container

--- pods/blabbering-container/index.js
+++ pods/blabbering-container/index.js
@@ -1,3 +1,9 @@
 setInterval(() => {
   console.log('Hi! I\'m a blabbering container!')
-}, 2000)
+}, 3000)
+
+process.stdin.resume()
+process.stdin.on('data',function(chunk) { 
+  let line = chunk.toString()
+  console.log('Yes, I totally agree with you regarding: ' + line)
+})
