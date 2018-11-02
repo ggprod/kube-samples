844854408ded0b97ef524966e4efa08aa13130fe
Mark Pevec <mark.pevec@gmail.com>
2018-11-01 20:14:41 -0400

    add a few more error types to blabbering-container

--- pods/blabbering-container/index.js
+++ pods/blabbering-container/index.js
@@ -3,6 +3,15 @@ setInterval(() => {
   console.log('Hi! I\'m a blabbering container!')
 }, 3000)
 
+setInterval(() => {
+  console.error('I\'m a random error!')
+}, 30000)
+
+setInterval(() => {
+  let structuredError = { type: 'structured', message: 'I\'m a structured error' } 
+  console.error(JSON.stringify(structuredError))
+}, 50000)
+
 process.stdin.resume()
 process.stdin.on('data',function(chunk) { 
   let line = chunk.toString()
